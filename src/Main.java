38ca135e8bdff74cac19350c6cd94a2ca2490b00
breba <netoferraz.rt@hotmail.com>
2017-02-20 10:53:54 -0300

    Commit de José Antonio

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+	public static void main(String[] args){
+		JOptionPane.showMessageDialog(null, "Bem-vindo ao sistema de mapeamento!");
+	}
+
+}
