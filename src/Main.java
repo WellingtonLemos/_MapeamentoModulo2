b1ccc5fce6f5b7dec582d8a486b88461b712839c
WellingtonLemos <Wellington@10.0.254.79>
2017-02-20 11:05:41 -0300

    Commit Feito - Wellington Lemos

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@ import javax.swing.JOptionPane;
 
 public class Main {
 	public static void main(String[] args){
+		
+		int n = 3;
+		for(int i=0; i<n; i++){
+			JOptionPane.showMessageDialog(null, n);
+			n--;
+		}
+		
 		JOptionPane.showMessageDialog(null, "Bem-vindo ao sistema de mapeamento!");
+		
 	}
 
 }
